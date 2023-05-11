0432b982db5301193c2b1f817bdf8a8901ef9633
ssingh-ss <91638382+ssingh-ss@users.noreply.github.com>
2023-05-11 20:54:15 +0530

    Update Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -134,13 +134,13 @@ export const Search: FC<SearchProps> = ({ onSearch, onAnswerUpdate, onDone }) =>
       {loading ? (
         <div className="flex items-center justify-center pt-64 sm:pt-72 flex-col">
           <div className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
-          <div className="mt-8 text-2xl">Getting answer...</div>
+          <div className="mt-8 text-2xl">Please wait Buddy...</div>
         </div>
       ) : (
         <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center space-y-6 px-3 pt-32 sm:pt-64">
           <div className="flex items-center">
             <IconBolt size={36} />
-            <div className="ml-1 text-center text-4xl">Detection</div>
+            <div className="ml-1 text-center text-4xl">FakeNews Detection</div>
           </div>
 
           {apiKey.length === 51 ? (
